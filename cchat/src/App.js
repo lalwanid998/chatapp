e895327e85159d64bac448b2abed46e1fe8263b2
lalwanid998 <lalwanid998@gmail.com>
2024-04-26 00:14:38 +0530

    first commit

---
+++ cchat/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import './App.css';
+import Join from './component/Join/Join';
+import Chat from "./component/Chat/Chat";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" Component={Join}></Route>
+          <Route path="/chat" Component={Chat}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
